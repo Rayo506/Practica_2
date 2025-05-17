07975990ff6bbbeced50276ac19cc509551a2cac
Carles Pelltiero <rayo506.charles@gmail.com>
2025-05-17 20:39:15 +0200

    update

--- Gestio_contingut.sql
+++ Gestio_contingut.sql
@@ -34,6 +34,7 @@ audio BLOB DEFAULT EMPTY_BLOB(),
 durada_segons number(2)
 );
 /
+
 --Crear una funció xml_producte(producte_id) que generi un document XML amb les dades del producte i referències a les imatges relacionades
 Create or replace procedure xml_producte (producte_id in number) as
  v_file BFILE;
@@ -41,32 +42,174 @@ Create or replace procedure xml_producte (producte_id in number) as
 BEGIN
  v_file := BFILENAME('DOCS_DIR', 'xml_dades');
  if dbms_lob.fileexists(v_file) = 1 Then
-    select contenido into v_clo from documentos where id = producte_id for update;
-    dmb_lob.open(v_file, dbms_lob.lob_readonly);
-    dmb_lob.loadfromfile(v_clo, v_file, dbms_lob.getlength(v_file));
-    dmb_lob.close(v_file);
+    select contenido into v_clo from DocumentsXML where id = producte_id for update;
+    dbms_lob.open(v_file, dbms_lob.lob_readonly);
+    dbms_lob.loadfromfile(v_clo, v_file, dbms_lob.getlength(v_file));
+    dbms_lob.close(v_file);
  end if;
 end;
 /
- 
-CREATE OR REPLACE PROCEDURE load_doc (p_id IN NUMBER, p_file IN VARCHAR2) AS
-  v_bfile BFILE;
-  v_clob CLOB;
+
+/*drop procedure xml_producte;*/
+
+CREATE OR REPLACE FUNCTION xml_producte(p_producte_id IN NUMBER) RETURN XMLType IS
+  v_xml XMLType;
+  v_count NUMBER;
 BEGIN
-  v_bfile := BFILENAME('DOCS_DIR', p_file);
-  IF DBMS_LOB.FILEEXISTS(v_bfile) = 1 THEN
-    SELECT contenido INTO v_clob FROM documentos WHERE id = p_id FOR UPDATE;
-    DBMS_LOB.OPEN(v_bfile, DBMS_LOB.LOB_READONLY);
-    DBMS_LOB.LOADFROMFILE(v_clob, v_bfile, DBMS_LOB.GETLENGTH(v_bfile));
-    DBMS_LOB.CLOSE(v_bfile);
-  END IF;
-END;
+  -- Verificar si el producto existe
+  SELECT COUNT(*) INTO v_count 
+  FROM ImatgesProducte 
+  WHERE producte_id = p_producte_id;
+  
+  IF v_count = 0 THEN
+    RETURN XMLType('<error>Producte no trobat</error>');
+  END IF;
+  
+  -- Generar el XML con los datos del producto y sus imágenes
+  SELECT XMLELEMENT("producte",
+           XMLATTRIBUTES(
+             p_producte_id AS "id",
+             (SELECT descripcio FROM ImatgesProducte WHERE producte_id = p_producte_id) AS "descripcio",
+             (SELECT format FROM ImatgesProducte WHERE producte_id = p_producte_id) AS "format"
+           ),
+           XMLELEMENT("imatges",
+             (SELECT XMLAGG(
+                        XMLELEMENT("imatge",
+                          XMLATTRIBUTES(
+                            producte_id AS "producte_id",
+                            descripcio AS "descripcio_imatge",
+                            format AS "format_imatge"
+                          )
+                        )
+                      )
+              FROM ImatgesProducte
+              WHERE producte_id = p_producte_id)
+           )
+         )
+  INTO v_xml
+  FROM dual;
+  
+  RETURN v_xml;
+EXCEPTION
+  WHEN OTHERS THEN
+    RETURN XMLType('<error>'||SQLERRM||'</error>');
+END xml_producte;
+/
+
 --para llamar
-?CALL load_doc(1, 'ia_articulo.txt');
+--CALL load_doc(1, 'ia_articulo.txt');
+
+begin
+  xml_producte(1);
+end;
+
+SELECT xml_producte(1) FROM dual;
 
 --Crear una taula DocumentsXML(id, xml_data XMLType) per emmagatzemar documents XML
-create or replace table DocumentsXML(
-id number primary key,
-xml_data XMLType
-);
+create table DocumentsXML(
+    id number primary key,
+    xml_data XMLType
+)XMLTYPE xml_data STORE AS BASICFILE BINARY XML;
 /
+
+
+--Implementar consultes amb XQuery per:
+--Obtenir imatges de productes amb format específic
+SELECT x.*
+FROM DocumentsXML d,
+     XMLTABLE(
+       '/producte/imatges/imatge[@format_imatge="png"]'
+       PASSING d.xml_data
+       COLUMNS
+         producte_id     NUMBER       PATH '@producte_id',
+         descripcio      VARCHAR2(250) PATH '@descripcio_imatge',
+         format_imatge   VARCHAR2(250) PATH '@format_imatge'
+     ) x;
+     
+SELECT x.*
+FROM DocumentsXML d,
+     XMLTABLE(
+       '/producte/imatges/imatge[@format_imatge="jpg"]'
+       PASSING d.xml_data
+       COLUMNS
+         producte_id     NUMBER       PATH '@producte_id',
+         descripcio      VARCHAR2(250) PATH '@descripcio_imatge',
+         format_imatge   VARCHAR2(250) PATH '@format_imatge'
+     ) x;
+
+--Llistar productes amb més d'una imatge
+SELECT id
+FROM DocumentsXML d
+WHERE XMLExists(
+  'count(/producte/imatges/imatge) > 1'
+  PASSING d.xml_data
+);
+
+
+--Inserciones para probar las tablas: 
+CREATE OR REPLACE DIRECTORY DOCS_DIR AS '/home/oracle/Documents';
+GRANT READ ON DIRECTORY DOCS_DIR TO PUBLIC;
+
+--inserts ImatgesProducte
+DECLARE
+  v_bfile BFILE;
+  v_blob  BLOB;
+BEGIN
+  -- Producto 1
+  INSERT INTO ImatgesProducte (producte_id, descripcio, imatge, format)
+  VALUES (1, 'Imatge de producte 1', EMPTY_BLOB(), 'png')
+  RETURNING imatge INTO v_blob;
+
+  v_bfile := BFILENAME('DOCS_DIR', 'imatge1.png');
+  dbms_lob.fileopen(v_bfile, dbms_lob.file_readonly);
+  dbms_lob.loadfromfile(v_blob, v_bfile, dbms_lob.getlength(v_bfile));
+  dbms_lob.fileclose(v_bfile);
+
+  -- Producto 2
+  INSERT INTO ImatgesProducte (producte_id, descripcio, imatge, format)
+  VALUES (2, 'Imatge de producte 2', EMPTY_BLOB(), 'jpg')
+  RETURNING imatge INTO v_blob;
+
+  v_bfile := BFILENAME('DOCS_DIR', 'imatge2.jpg');
+  dbms_lob.fileopen(v_bfile, dbms_lob.file_readonly);
+  dbms_lob.loadfromfile(v_blob, v_bfile, dbms_lob.getlength(v_bfile));
+  dbms_lob.fileclose(v_bfile);
+  COMMIT;
+END;
+/
+--inserts AudiosComanda
+DECLARE
+  v_bfile BFILE;
+  v_blob  BLOB;
+BEGIN
+  -- Comanda 1
+  INSERT INTO AudiosComanda (comanda_id, audio, durada_segons)
+  VALUES (1, EMPTY_BLOB(), 15)
+  RETURNING audio INTO v_blob;
+
+  v_bfile := BFILENAME('DOCS_DIR', 'audio1.mp3');
+  dbms_lob.fileopen(v_bfile, dbms_lob.file_readonly);
+  dbms_lob.loadfromfile(v_blob, v_bfile, dbms_lob.getlength(v_bfile));
+  dbms_lob.fileclose(v_bfile);
+
+  -- Comanda 2
+  INSERT INTO AudiosComanda (comanda_id, audio, durada_segons)
+  VALUES (2, EMPTY_BLOB(), 25)
+  RETURNING audio INTO v_blob;
+
+  v_bfile := BFILENAME('DOCS_DIR', 'audio2.wav');
+  dbms_lob.fileopen(v_bfile, dbms_lob.file_readonly);
+  dbms_lob.loadfromfile(v_blob, v_bfile, dbms_lob.getlength(v_bfile));
+  dbms_lob.fileclose(v_bfile);
+  COMMIT;
+END;
+/
+--XML
+INSERT INTO DocumentsXML (id, xml_data)
+VALUES (1, xml_producte(1));
+
+INSERT INTO DocumentsXML (id, xml_data)
+VALUES (2, xml_producte(2));
+COMMIT;
+--
+
---
+++ multimedia_tools.py
@@ -0,0 +1,6 @@
+#6. Desenvolupar el script multimedia_tools.py que:
+#• Carrega imatges (.jpg) i àudios (.mp3) des del sistema de fitxers.
+#• Desa els fitxers com a BLOB a les respectives taules.
+#• Desa documents XML a la taula DocumentsXML.
+#• Executa consultes XQuery sobre els documents XML i mostra els resultats.
+#• Consulta la mida dels fitxers desats i mostra un resum.
